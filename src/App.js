abe08ea1d0d032c48e5000398e123f21c2cbc6e8
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-10-11 15:06:48 +0900

    routeの追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
