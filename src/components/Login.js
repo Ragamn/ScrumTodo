58e793bd9ee2dfdf78b0dadd20bac6ff3802ac7b
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-10-11 15:06:35 +0900

    ログイン処理の作成

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,21 @@ import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+const Login = () => {
+  const navigate = useNavigate(); // useNavigateフックを使用
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value);
+      navigate("/"); // history.pushの代わりにnavigateを使用
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
 
   return (
     <div>
