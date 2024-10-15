be192a10c6f181ce53fae153cf4d4ce9b7bf55b0
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-10-15 10:09:30 +0900

    ログアウトのボタン生成

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/Signup";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/" element={<Login />} />
-          </Routes>
+          <div style={{ margin: "0,2em" }}>
+            {/* <Navbar /> */}
+            <Routes>
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/" element={<Login />} />
+            </Routes>
+          </div>
         </BrowserRouter>
       </AuthProvider>
     </div>
